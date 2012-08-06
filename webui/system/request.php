7cd3c12070993f34e38ad41df9db077e9c5e982e
SJ <sj@acts.hu>
2012-08-06 12:46:02 +0200

    fixed a reference call bug in system/request.php

--- webui/system/request.php
+++ webui/system/request.php
@@ -10,33 +10,17 @@ class Request {
 
 
    public function __construct() {
-       $this->get    =& $this->clean($_GET);
-       $this->post   =& $this->clean($_POST);
-       $this->cookie =& $this->clean($_COOKIE);
-       $this->files  =& $this->clean($_FILES);
-       $this->server =& $this->clean($_SERVER);
+       $this->get    = $_GET;
+       $this->post   = $_POST;
+       $this->cookie = $_COOKIE;
+       $this->files  = $_FILES;
+       $this->server = $_SERVER;
 
        if(isset($_SERVER['REQUEST_URI'])) {
           $this->fixed_request_uri = preg_replace("/\/([\w]+)\.php\?{0,1}/", "", $_SERVER['REQUEST_URI']);
        }
-
    }
-
-
-   public function clean($data) {
-      if (is_array($data)) {
-         foreach ($data as $key => $value) {
-            $data[$key] =& $this->clean($value);
-         }
-      } else {
-          //$data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
-      }
-
-      return $data;
-   }
-
 
 }
-
 
 ?>
