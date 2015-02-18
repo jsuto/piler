c1e525b931988484ab9e59664b913c600e2eae77
SJ <sj@acts.hu>
2015-02-18 15:51:38 +0100

    improved cpu usage command

--- webui/config.php
+++ webui/config.php
@@ -200,7 +200,7 @@ $config['SPHINX_NOTE_INDEX'] = 'note1';
 
 $config['RELOAD_COMMAND'] = 'sudo -n /etc/init.d/rc.piler reload';
 $config['PILERIMPORT_IMAP_COMMAND'] = '/usr/local/bin/pilerimport -d /var/piler/imap -q -r';
-$config['CPU_USAGE_COMMAND'] = "LC_ALL=C mpstat | tail -1 | sed 's/\ \{1,\}/ /g' | rev | cut -d ' ' -f1 | rev";
+$config['CPU_USAGE_COMMAND'] = "LC_ALL=C mpstat | tail -1 | rev | awk '{ print $1 }' | rev";
 
 $config['LDAP_IMPORT_CONFIG_FILE'] = '/usr/local/etc/ldap-import.cfg';
 
