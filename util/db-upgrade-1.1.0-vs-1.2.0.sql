fe78d817d4ad87af6a2eae26a21b0d15ed4ae270
SJ <sj@acts.hu>
2015-03-18 15:23:21 +0100

    fixed sql upgrade script

--- util/db-upgrade-1.1.0-vs-1.2.0.sql
+++ util/db-upgrade-1.1.0-vs-1.2.0.sql
@@ -3,6 +3,9 @@
 alter table archiving_rule add column `body` varchar(128) default null;
 alter table retention_rule add column `body` varchar(128) default null;
 
+--drop index entry on archiving_rule;
+--drop index entry on retention_rule;
+
 create unique index `entry` on archiving_rule (`domain`,`from`,`to`,`subject`,`body`,`_size`,`size`,`attachment_name`,`attachment_type`,`_attachment_size`,`attachment_size`,`spam`);
 create unique index `entry` on retention_rule (`domain`,`from`,`to`,`subject`,`body`,`_size`,`size`,`attachment_name`,`attachment_type`,`_attachment_size`,`attachment_size`,`spam`);
 
