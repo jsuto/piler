b4867d79fe3d398b7f1a80603cc659be8f1cbcb8
SJ <sj@acts.hu>
2015-05-22 11:37:08 +0200

    fix a counter issue

--- webui/model/stat/counter.php
+++ webui/model/stat/counter.php
@@ -4,22 +4,7 @@ class ModelStatCounter extends Model {
 
    public function get_counters(){
       $counter = array();
-      $asize = 0;
-
-      if(MEMCACHED_ENABLED) {
-         $memcache = Registry::get('memcache');
-
-         $counter = $memcache->get(Registry::get('counters'));
-
-         if(isset($counter[MEMCACHED_PREFIX . 'counters_last_update'])) {
-            if(isset($counter[MEMCACHED_PREFIX . 'size'])) { $asize = $counter[MEMCACHED_PREFIX . 'size']; }
-            if(isset($counter[MEMCACHED_PREFIX . 'stored_size'])) { $ssize = $counter[MEMCACHED_PREFIX . 'stored_size']; }
-            unset($counter[MEMCACHED_PREFIX . 'size']);
-            unset($counter[MEMCACHED_PREFIX . 'stored_size']);
-
-            return array ($asize, $ssize, $counter);
-         }
-      }
+      $asize = $ssize = 0;
 
       $query = $this->db->query("SELECT * FROM " . TABLE_COUNTER);
 
