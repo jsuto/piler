74b761e70d25ab8c72e3acf908452d0bf4f6f5c6
SJ <sj@acts.hu>
2015-09-04 10:56:10 +0200

    added missing table to db upgrade script

--- util/db-upgrade-1.1.0-vs-1.2.0.sql
+++ util/db-upgrade-1.1.0-vs-1.2.0.sql
@@ -32,4 +32,27 @@ create table if not exists `timestamp` (
 alter table archiving_rule add column `folder_id` int default 0;
 alter table retention_rule add column `folder_id` int default 0;
 
+-- 2015.09.04
+
+create table if not exists `folder_rule` (
+   `id` bigint unsigned not null auto_increment,
+   `domain` varchar(128) default null,
+   `from` varchar(128) default null,
+   `to` varchar(128) default null,
+   `subject` varchar(128) default null,
+   `body` varchar(128) default null,
+   `_size` char(2) default null,
+   `size` int default 0,
+   `attachment_name` varchar(128) default null,
+   `attachment_type` varchar(128) default null,
+   `_attachment_size` char(2) default null,
+   `attachment_size` int default 0,
+   `spam` tinyint(1) default -1,
+   `days` int default 0,
+   `folder_id` int default 0,
+   primary key (`id`),
+   unique (`domain`,`from`,`to`,`subject`,`body`,`_size`,`size`,`attachment_name`,`attachment_type`,`_attachment_size`,`attachment_size`,`spam`)
+) ENGINE=InnoDB;
+
+
 
