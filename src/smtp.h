79d7e1fb56ab5bf4400191874950f15a748c98d7
Janos SUTO <sj@acts.hu>
2016-08-31 10:37:13 +0000

    smtp.h edited online with Bitbucket

--- src/smtp.h
+++ src/smtp.h
@@ -5,14 +5,14 @@
 #ifndef _SMTP_H
  #define _SMTP_H
 
-void process_command_ehlo_lhlo(struct session_ctx *sctx, struct session_data *sdata, int *protocol_state, char *resp, int resplen);
-void process_command_starttls(struct session_ctx *sctx, struct session_data *sdata, int *protocol_state, int *starttls, char *resp, int resplen);
-void process_command_mail_from(struct session_data *sdata, int *protocol_state, char *buf, char *resp, int resplen, struct __config *cfg);
-void process_command_rcpt_to(struct session_data *sdata, int *protocol_state, char *buf, char *resp, int resplen);
-void process_command_data(struct session_data *sdata, int *protocol_state, char *resp, int resplen);
+void process_command_ehlo_lhlo(struct session_ctx *sctx, int *protocol_state, char *resp, int resplen);
+void process_command_starttls(struct session_ctx *sctx, int *protocol_state, int *starttls, char *resp, int resplen);
+void process_command_mail_from(struct session_ctx *sctx, int *protocol_state, char *buf, char *resp, int resplen);
+void process_command_rcpt_to(struct session_ctx *sctx, int *protocol_state, char *buf, char *resp, int resplen);
+void process_command_data(struct session_ctx *sctx, int *protocol_state, char *resp, int resplen);
 void process_command_bdat(struct session_ctx *sctx, int *protocol_state, char *buf, char *resp, int resplen);
-void process_command_quit(struct session_data *sdata, int *protocol_state, char *resp, int resplen, struct __config *cfg);
-void process_command_reset(struct session_data *sdata, int *protocol_state, char *resp, int resplen, struct __config *cfg);
+void process_command_quit(struct session_ctx *sctx, int *protocol_state, char *resp, int resplen);
+void process_command_reset(struct session_ctx *sctx, int *protocol_state, char *resp, int resplen);
 
 int read_bdat_data(struct session_ctx *sctx, int expected_bdat_len);
 int extract_bdat_command(struct session_ctx *sctx, char *buf);
