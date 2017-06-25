2dc31b59c03054b9863b999bbefaf030294f5e1d
Janos SUTO <sj@acts.hu>
2017-06-25 22:05:06 +0200

    improving bitbucket pipeline
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- unit_tests/run.sh
+++ unit_tests/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -o errexit
+set -o pipefail
+set -o nounset
+set -x
+
 LD_LIBRARY_PATH=../src ./check_parser_utils
 LD_LIBRARY_PATH=../src ./check_parser
 LD_LIBRARY_PATH=../src ./check_rules
 LD_LIBRARY_PATH=../src ./check_digest
 LD_LIBRARY_PATH=../src ./check_mydomains
-
-
