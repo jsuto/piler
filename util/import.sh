3b3d0157caa5b075255672ef991015b330d690f0
Janos SUTO <sj@acts.hu>
2020-10-27 22:20:03 +0100

    Fixed import.sh
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- util/import.sh
+++ util/import.sh
@@ -4,19 +4,11 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-TMPFILE=/var/run/piler/import.tmp
+export PATH=$PATH:/usr/libexec/piler:/usr/local/libexec/piler
 
-if [[ -f $TMPFILE ]]; then exit 1; fi
+pushd /var/piler/imap
 
-date > $TMPFILE
-
-function finish {
-   rm -f $TMPFILE
-}
-
-trap finish EXIT
-
-cd /var/piler/imap
-
-pilerimport -G >/dev/null
+exec 200 > "/tmp/${0}-lock" || exit 1
+flock 200 || exit 1
+imapfetch.py -i
+flock -u 200
