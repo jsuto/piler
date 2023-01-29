ebab0eabcc022f5f2e92e78d8534455f42059933
Janos SUTO <sj@acts.hu>
2023-01-29 17:55:41 +0100

    Fixed issue #1058 renamed table group to usergroup
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- util/db-upgrade.sql
+++ util/db-upgrade.sql
@@ -6,3 +6,9 @@ alter table sph_index change column todomain todomain blob(512) default null;
 
 
 alter table metadata change column subject `subject` blob(512) default null;
+
+
+SELECT Count(*) INTO @exists FROM information_schema.tables WHERE table_schema = 'piler' AND table_type = 'BASE TABLE' AND table_name = 'group';
+SET @query = If(@exists>0, 'RENAME TABLE `group` TO usergroup', 'SELECT 1 from dual');
+PREPARE stmt FROM @query;
+EXECUTE stmt;
