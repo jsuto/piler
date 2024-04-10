e2bb809ec179b0b02fb6163c1d918e47efb53d79
Janos SUTO <sj@acts.hu>
2024-04-10 05:53:45 +0200

    Run tests
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- tests/setup.sh
+++ tests/setup.sh
@@ -8,6 +8,7 @@ SCRIPT_PATH="$(readlink -f "$0")"
 SCRIPT_DIR="${SCRIPT_PATH%/*}"
 
 pushd "$SCRIPT_DIR"
+echo -e "$CONFIG_SITE_PHP" | docker exec -i "$CONTAINER" bash -c "cat >> /etc/piler/config-site.php"
 docker cp setup.sql "${CONTAINER}:/tmp"
 docker exec "$CONTAINER" bash -c 'mysql --defaults-file=/etc/piler/.my.cnf < /tmp/setup.sql'
 docker exec "$CONTAINER" /etc/init.d/rc.piler reload
