f2b5612cadfc41a13ed4765c21383a62b985b1d8
jsuto <sj@acts.hu>
2024-05-08 20:30:50 +0200

    Fixed test workflow (#33)
    Signed-off-by: Janos SUTO <sj@acts.hu>

---
+++ tests/config-site-extras.php
@@ -0,0 +1,11 @@
+$config['TSA_URL'] = 'https://freetsa.org/tsr';
+$config['TSA_PUBLIC_KEY_FILE'] = '/etc/piler/tsa.pem';
+$config['TSA_START_ID'] = 1;
+$config['TSA_STAMP_REQUEST_UNIT_SIZE'] = 2892;
+$config['TSA_VERIFY_CERTIFICATE'] = false;
+$config['ENABLE_IMAP_AUTH'] = 1;
+$config['IMAP_HOST'] = 'imap.aaa.fu';
+$config['IMAP_PORT'] =  143;
+$config['IMAP_SSL'] = false;
+$config['MEMCACHED_ENABLED'] = 1;
+$memcached_server = ['memcached', 11211];
