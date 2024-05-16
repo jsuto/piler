32b5c4c3739bd03a436c79d4e613b4d954bdc0cb
Janos SUTO <sj@acts.hu>
2024-05-16 05:06:22 +0200

    Updated docker build.sh
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- docker/build.sh
+++ docker/build.sh
@@ -4,8 +4,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-IMAGE_NAME="sutoj/piler:1.4.4"
+IMAGE_NAME="sutoj/piler:test"
 
 if [[ $# -ne 1 ]]; then echo "ERROR: missing package name" 1>&2; exit 1; fi
 
-docker build --pull --build-arg PACKAGE="$1" -t "$IMAGE_NAME" .
+docker buildx build --build-arg PACKAGE="$1" -t "$IMAGE_NAME" .
