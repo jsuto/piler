e66d42ef2c015ee2dd545da339a8f66bbc4f4167
Janos SUTO <sj@acts.hu>
2024-05-19 09:57:43 +0200

    Added more goodies to container
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- tests/run.sh
+++ tests/run.sh
@@ -209,5 +209,7 @@ run_import_job
 docker exec "$CONTAINER" tail -30 /var/log/nginx/error.log
 
 docker exec "$CONTAINER" bash -c 'apt-get update && apt-get install -y vim'
+docker exec -i "$CONTAINER" bash -c 'cat >>/root/.bashrc' <<< "alias n='tail -f /var/log/nginx/error.log'"
+docker exec -i "$CONTAINER" bash -c 'cat >>/root/.bashrc' <<< "alias t='tail -f /var/log/mail.log'"
 
 get_verdict
